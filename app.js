265c9872045852da191e704d148d3d5495bde894
aman <amandon.pathak12@gmail.com>
2024-06-06 00:08:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function openNav() {
+	document.getElementById("mySidenav").style.display = "block";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.display = "none";
+}
+
+// Hero Video play on hover
+const heroBgVid = document.querySelector(".slideshow span.hero-vid");
+const slideshow = document.querySelector(".slideshow");
+
+slideshow.addEventListener("mouseover", () => {
+	heroBgVid.style.zIndex = "4";
+});
+slideshow.addEventListener("mouseleave", () => {
+	heroBgVid.style.zIndex = "1";
+});
+
+// overlay
+const overlay = document.querySelector(".overlay");
+const hamburgerIcon = document.querySelector(".flex-column span:last-of-type");
+
+// hamburgerIcon.addEventListener("click", () => {
+// 	overlay.classList.add("active");
+// });
